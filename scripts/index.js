ff5ca902af35a7c0aaab202b3d2133677b65f0bd
ABOBA123 <tarasovlev7@gmail.com>
2022-08-03 02:00:00 +0300

    скоро адаптивы подъедут

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,23 @@ window.addEventListener('scroll', function () {
         threeButton.className = "three-button off"
     }
 })
+
+const men = document.getElementById('men')
+ function menactive (){
+
+   men.className='men  men-active'
+
+ }
+ setTimeout(menactive, 1500);
+
+ const slide = document.getElementById('third-slide')
+ const FetText = document.getElementById('fet')
+ window.addEventListener('scroll',function(){
+    // console.log(this.window.pageYOffset, thirdSlide.scrollHeight)
+    if (window.pageYOffset >= 608) {
+        FetText.className='fat-text'
+    }
+    else {
+        FetText.className ="fat-text off_fat-text"
+    }
+ })
